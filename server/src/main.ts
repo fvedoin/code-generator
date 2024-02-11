7b72ff9bec965b91b794b94c4aeb595f73b5a992
Fernando <fernando.vgarcia@hotmail.com>
2024-02-11 20:21:07 +0000

    feat: adding docker support & fallback to environment variables

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.CORS_ORIGIN });
+  const originFallback = '*';
+  app.enableCors({ origin: process.env.CORS_ORIGIN ?? originFallback });
   await app.listen(3000);
 }
 bootstrap();
--- ui/src/services/api.ts
+++ ui/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiFallbackURL = 'http://localhost:3000';
+
 const apiService = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_BASE_URL,
+  baseURL: import.meta.env.VITE_REACT_APP_BASE_URL ?? apiFallbackURL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
